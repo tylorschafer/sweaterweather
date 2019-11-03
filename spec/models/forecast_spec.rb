da80310bc5dce6531b0ef9fed6d3a7672506751a
Tylor Schafer <tylorschafer@gmail.com>
2019-11-03 12:59:08 -0700

    Add forecast model

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -7,35 +7,35 @@ describe Forecast do
     results = JSON.parse(dummy_response, symbolize_names: true)
     forecast = Forecast.new(results)
 
-    expect(forecast).to be a Forecast
+    expect(forecast).to be_a Forecast
     expect(forecast.current).to be_a Hash
-    expect(forecast.current[:temp]).to exist
-    expect(forecast.current[:icon]).to exist
-    expect(forecast.current[:summary]).to exist
-    expect(forecast.current[:dal_temp_low]).to exist
-    expect(forecast.current[:dal_temp_high]).to exist
-    expect(forecast.current[:time]).to exist
-    expect(forecast.current[:timezone]).to exist
-    expect(forecast.current[:app_temp]).to exist
-    expect(forecast.current[:humidity]).to exist
-    expect(forecast.current[:visibility]).to exist
-    expect(forecast.current[:day_summary]).to exist
-    expect(forecast.current[:night_summary]).to exist
-    expect(forecast.current[:uv]).to exist
+    expect(forecast.current[:temp]).to be_a Float
+    expect(forecast.current[:icon]).to be_a String
+    expect(forecast.current[:summary]).to be_a String
+    expect(forecast.current[:dal_temp_low]).to be_a Float
+    expect(forecast.current[:dal_temp_high]).to be_a Float
+    expect(forecast.current[:time]).to be_a String
+    expect(forecast.current[:timezone]).to be_a String
+    expect(forecast.current[:app_temp]).to be_a Float
+    expect(forecast.current[:humidity]).to be_a Float
+    expect(forecast.current[:visibility]).to be_a Integer
+    expect(forecast.current[:day_summary]).to be_a String
+    expect(forecast.current[:night_summary]).to be_a String
+    expect(forecast.current[:uv]).to be_an Integer
 
     expect(forecast.next_8_hrs).to be_an Array
-    expect(forecast.next_8_hrs).count to eq(8)
+    expect(forecast.next_8_hrs.count).to eq(8)
     expect(forecast.next_8_hrs[0]).to be_a Hash
-    expect(forecast.next_8_hrs[0][:icon]).to exist
-    expect(forecast.next_8_hrs[0][:temp]).to exist
-    expect(forecast.next_8_hrs[0][:time]).to exist
+    expect(forecast.next_8_hrs[0][:icon]).to be_a String
+    expect(forecast.next_8_hrs[0][:temp]).to be_a Float
+    expect(forecast.next_8_hrs[0][:time]).to be_a String
 
-    expect(forecast.weekly).to be_an Array
-    expect(forecast.weekly).count to eq(5)
-    expect(forecast.weekly[0]).to be_a Hash
-    expect(forecast.weekly[0][:icon]).to exist
-    expect(forecast.weekly[0][:percip_prob]).to exist
-    expect(forecast.weekly[0][:temp_high]).to exist
-    expect(forecast.weekly[0][:temp_low]).to exist
+    expect(forecast.next_5_days).to be_an Array
+    expect(forecast.next_5_days.count).to eq(5)
+    expect(forecast.next_5_days[0]).to be_a Hash
+    expect(forecast.next_5_days[0][:icon]).to be_a String
+    expect(forecast.next_5_days[0][:percip_prob]).to be_a Float
+    expect(forecast.next_5_days[0][:temp_high]).to be_a Float
+    expect(forecast.next_5_days[0][:temp_low]).to be_a Float
   end
 end
