4578b29fc0fc70f82dee40359cfa0e789123fd45
Tylor Schafer <tylorschafer@gmail.com>
2019-11-04 09:19:12 -0700

    Add antipodes controller

---
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::AntipodesController < ApplicationController
+  def show
+    coordinates = GoogleGeocoding.coordinates(params[:location])
+    lat = coordinates[:data][:lat]
+    long = coordinates[:data][:lng]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'forecast', to: 'forecasts#show'
       get 'backgrounds', to: 'backgrounds#index'
+      get 'antipode', to: 'antipodes#show'
+
       devise_for :users, controllers: { sessions: :sessions }
       post 'login', to: 'sessions#create'
     end
