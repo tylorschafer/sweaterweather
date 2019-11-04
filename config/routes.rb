e8d9f86f27e11657f3cf71f848d9d1b4126c4041
Tylor Schafer <tylorschafer@gmail.com>
2019-11-03 22:08:42 -0700

    Add better routes for session create

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
     namespace :v1 do
       get 'forecast', to: 'forecasts#show'
       get 'backgrounds', to: 'backgrounds#index'
-
-      resource :user, only: [:show, :update]
-
-      resource :sessions, only: [:create]
+      devise_for :users, controllers: { sessions: :sessions }
+      post 'login', to: 'sessions#create'
     end
   end
 end
