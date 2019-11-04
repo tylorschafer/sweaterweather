fe4da7ab92b280ae3af076458d73c266ce4f02b5
Tylor Schafer <tylorschafer@gmail.com>
2019-11-03 19:05:51 -0700

    Add token generator to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def generate_token
+    JWT.encode({ id: id }, Rails.application.secrets.secret_key_base)
+  end
 end
