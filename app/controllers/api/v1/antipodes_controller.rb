f58f145900c538a84bbd9a89b4c2fed5ada9e76c
Tylor Schafer <tylorschafer@gmail.com>
2019-11-04 10:41:58 -0700

    Refactor antipodes_controller

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -1,13 +1,26 @@
 class Api::V1::AntipodesController < ApplicationController
   def show
-    coordinates = GoogleGeocoding.coordinates(params[:location])
-    amy = Amypode.find_antipode(coordinates[:data][:lat], coordinates[:data][:lng])
-    found_city = GoogleGeocoding.reverse_lookup(
-      amy[:data][:attributes][:lat],amy[:data][:attributes][:long]
-    )
-    forecast = Darksky.new(amy[:data][:attributes][:lat],amy[:data][:attributes][:long])
-      .create_forecast
+    pode = find_antipode
+    lat = pode[:data][:attributes][:lat]
+    long = pode[:data][:attributes][:long]
+    found_city = find_city(lat, long)
+    forecast = find_forecast(lat, long)
     render json: AntipodeSerializer.new(found_city, forecast, params[:location])
       .serialized_json
+  end
+
+  def find_forecast(lat, long)
+    Darksky.new(lat, long).create_forecast
+  end
+
+  def find_city(lat, long)
+    GoogleGeocoding.reverse_lookup(lat, long)
+  end
+
+  def find_antipode
+    coordinates = GoogleGeocoding.coordinates(params[:location])
+    lat = coordinates[:data][:lat]
+    long = coordinates[:data][:lng]
+    Amypode.find_antipode(lat, long)
   end
 end
