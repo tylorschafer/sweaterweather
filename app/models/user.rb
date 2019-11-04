372ea46784c939391ca6dda19b66ff5aaedab0d7
Tylor Schafer <tylorschafer@gmail.com>
2019-11-03 19:09:41 -0700

    Add user json partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, uniqueness: { case_sensitive: false },
+            presence: true,
+            allow_blank: false
+
   def generate_token
     JWT.encode({ id: id }, Rails.application.secrets.secret_key_base)
   end
