f22d6fc0e5b8267bb70a99ee9c5bb558e645e5c0
Tylor Schafer <tylorschafer@gmail.com>
2019-11-04 09:16:18 -0700

    Add antipode request spec

---
+++ spec/requests/antipode_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Antipode endpoint' do
+  it 'Returns the antipode city and forecast for that city' do
+    get '/api/v1/antipode?location=hong+kong'
+
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(results).to be_a Hash
+    expect(results[:data]).to be_a Hash
+    expect(results[:data][:type]).to eq('antipode')
+    expect(results[:data][:attributes]).to be_a Hash
+    expect(results[:data][:attributes][:location_name]).to be_a String
+    expect(results[:data][:attributes][:forecast]).to be_a Hash
+    expect(results[:data][:attributes][:forecast][:summary]).to be_a String
+    expect(results[:data][:attributes][:forecast][:current_temperature]).to be_a String
+    expect(results[:data][:search_location]).to be_a String
+  end
+end
