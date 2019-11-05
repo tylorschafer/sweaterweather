8b41b9e164d474d8c3a7f53e3b83da9e892c18bc
Tylor Schafer <tylorschafer@gmail.com>
2019-11-04 19:25:49 -0700

    Add trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,8 @@
-class Trip 
-
+class Trip
+  attr_reader :distance, :duration, :weather_summary
+  def initialize(params = {})
+    @distance = params[:distance]
+    @duration = params[:duration]
+    @weather_summary = params[:weather_summary]
+  end
 end
