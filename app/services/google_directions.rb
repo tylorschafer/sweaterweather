bc7ab735570e8fb9eafbece5ecbc8912394037b6
Tylor Schafer <tylorschafer@gmail.com>
2019-11-04 19:12:54 -0700

    Add google directions service

---
+++ app/services/google_directions.rb
@@ -0,0 +1,17 @@
+class GoogleDirections
+
+  def self.trip_info(origin, destination)
+    new.find_trip(origin, destination)
+  end
+
+  def find_trip(origin, destination)
+    response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json") do |f|
+      f.params = {
+        key: ENV['GOOGLE_API_KEY'],
+        origin: origin,
+        destination: destination
+      }
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
--- spec/services/google_directions_spec.rb
+++ spec/services/google_directions_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe GoogleDirections do
-  xit 'Returns travel time for a trip' do
-    trip = GoogleDirections.trip_info({ origin: "Denver,CO", destination: "Pueblo,CO" })
+  it 'Returns travel time for a trip' do
+    trip = GoogleDirections.trip_info("Denver,CO", "Pueblo,CO")
 
     expect(trip).to be_a Hash
-    expect(trip[:routes][:legs][:duration][:text]).to be_a String
+    expect(trip[:routes][0][:legs][0][:duration][:text]).to be_a String
   end
 end
