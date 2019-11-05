330763a5049e9a2d480a3b84325cc3c9e5c627d5
Tylor Schafer <tylorschafer@gmail.com>
2019-11-05 09:28:06 -0700

    Fix forecasts controller layout

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::ForecastsController < ApplicationController
-
   def show
     facade = ForecastsFacade.new(params[:location])
     render json: ForecastSerializer.new(facade.forecast)
